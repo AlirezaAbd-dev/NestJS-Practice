29bb99bdd263be2ca97681053299f71fc24498a7
0AliReza0 <alireza.abedi9310@gmail.com>
2024-05-02 13:30:49 +0330

    changed.

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,11 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TagEntity } from '@app/tag/tag.entity';
 
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
+  password: process.env.DATABASE_PASSWORD,
   database: 'mediumclone',
+  entities: [TagEntity],
+  synchronize: true,
 };
-
 export default config;
